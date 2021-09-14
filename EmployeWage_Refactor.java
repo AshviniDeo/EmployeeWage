bdfc4ddad4a9647a957ac129458bfa319c589cc8
Ashvini Deokate <89019792+AshviniDeo@users.noreply.github.com>
2021-09-15 03:32:14 +0530

    Add files via upload

---
+++ EmployeWage_Refactor.java
@@ -0,0 +1,54 @@
+package assignment8;
+
+public class EmployeWage_Refactor {
+	public int SwtchCase(int check) {
+		int workHour = 0;
+		switch (check){
+        case 0 :
+            workHour = workHour + 4;
+            break;
+        case 1 :
+            workHour = workHour + 8;
+        default:
+    }
+    return workHour;
+	}
+	public int WorkingHour(int workingHour,int totalWorkHour,int workingDay,int totalWorkingDay,int workHour) {
+		 while (workingHour == totalWorkHour || workingDay == totalWorkingDay){
+             workingDay++;
+             workingHour = (workingHour + workHour);
+         }
+		 return workingHour;
+	}
+	public int Check() {
+		 
+		 int check = (int) Math.floor(Math.random() * 2);
+         if (check == 0){
+             System.out.println("PartTime Employee");
+         }else {
+             System.out.println("FullTime Employee");
+         }
+         return check;
+	}
+	public void Calculate(int wagePrHour,int workHour,int totalWorkHour) {
+		int employedailyWage = (wagePrHour * workHour);
+        int employeMonthlyWage = (employedailyWage * totalWorkHour);
+        System.out.println("Employee Monthly Wage is :" + employeMonthlyWage);
+	}
+	 public static void main(String[] args) {
+         int wagePrHour = 20;
+         int totalWorkingDay = 20;
+         
+         int totalWorkHour = 100;
+         int workingDay = 0;
+         int workingHour = 0;
+        EmployeWage_Refactor obj = new EmployeWage_Refactor();
+       int workHr = obj.SwtchCase( obj.Check());
+        obj.WorkingHour(workingHour, totalWorkHour, workingDay, totalWorkingDay, totalWorkHour);
+        obj.Calculate(wagePrHour, workHr, totalWorkHour);
+             
+
+
+     }
+
+}
