32215e9628cc74e4fc7b9a69c69df98a1f005dc8
Ashvini Deokate <89019792+AshviniDeo@users.noreply.github.com>
2021-09-26 03:45:08 +0530

    Interface empWage Builder program to company created

---
+++ EmployeWageBuilder.java
@@ -0,0 +1,22 @@
+package employeWadge;
+
+import java.util.Scanner;
+
+public interface EmployeWageBuilder {
+	public static int attendence() {
+		return 0;}
+
+	public static int check() {
+		return 0;}
+	
+	public static int switchCase() {
+		return 0;}
+	
+	public static int workingHour() {
+		return 0;}
+	
+	public static int calculate() {
+		return 0;}
+	public static void display() {}
+
+}
